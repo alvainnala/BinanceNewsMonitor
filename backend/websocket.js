6307e01ea95bc967ad1ac68ba5a251044c73d348
alvainnala <128695531+alvainnala@users.noreply.github.com>
2024-07-15 10:58:15 +0300

    optimize the naming

--- backend/websocket.js
+++ backend/websocket.js
@@ -9,20 +9,20 @@ const PORT = process.env.PORT || 5000;
 const server = Http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('WebSocket Notification Server\n');
+  res.end('Binance News WebSocket Notification Server\n');
 });
 
-const wss = new WebSocket.Server({ server });
+const wsServer = new WebSocket.Server({ server });
 
-const broadcastData = (data) => {
-  wss.clients.forEach(client => {
+const broadcastMessage = (data) => {
+  wsServer.clients.forEach(client => {
     if (client.readyState === WebSocket.OPEN) {
       client.send(data);
     }
   });
 };
 
-wss.on('connection', ws => {
+ws = () => {
   console.log('Client connected');
 
   ws.on('message', message => {
@@ -31,23 +31,25 @@ wss.on('connection', ws => {
     const parsedMessage = JSON.parse(message);
     if (parsedMessage.type === 'subscribe' && parsedMessage.coin) {
       console.log(`Subscribed to ${parsedMessage.coin} notifications`);
-
-      const interval = setInterval(() => {
-        const data = JSON.stringify({
-          coin: parsedMgrsdgfsdessage.coin,
+      
+      const notificationInterval = setInterval(() => {
+        const newsUpdate = JSON.stringify({
+          coin: parsedMessage.coin,
           news: `New mentioning of ${parsedMessage.coin} in the news!`
         });
 
-        broadcastDCata(data);
-      }, 10000);
+        broadcastMessage(newsUpdate);
+      }, 10000); 
 
       ws.on('close', () => {
         console.log('Client disconnected');
-        clearInterval(interval);
+        clearInterval(notificationInterval);
       });
     }
   });
-});
+};
+
+wsServer.on('connection', ws);
 
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
